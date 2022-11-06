17ee471b180420f2e7451ced272fea0d59451042
vovietl0ng <vovielong123@gmail.com>
2022-11-06 21:34:10 +0700

    logbook2

--- app/src/main/java/com/example/logbook/MainActivity.java
+++ app/src/main/java/com/example/logbook/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.logbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 import java.util.ArrayList;
@@ -14,7 +18,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
-    Button Backward , Nextward;
+    EditText input;
+    Button Backward ,Nextward,Add_Url;
     Picasso picasso;
     List<String> listImage;
     int i = 1;
@@ -24,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView2);
         Backward = findViewById(R.id.button);
+        input = findViewById(R.id.inputURL);
+        Add_Url = findViewById(R.id.button_add);
+        Add_Url.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = input.getText().toString().trim();
+                Add_Image(url);
+            }
+        });
         Backward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,12 +61,31 @@ public class MainActivity extends AppCompatActivity {
         });
         picasso = new Picasso.Builder(this).build();
         listImage = new ArrayList<>();
-        listImage.add("https://tinyurl.com/ubedmu4m");
-        listImage.add("https://tinyurl.com/2fehtenc");
-        listImage.add("https://tinyurl.com/33dm7erv");
-        showImage(i);
+//        listImage.add("https://tinyurl.com/ubedmu4m");
+//        listImage.add("https://tinyurl.com/2fehtenc");
+//        listImage.add("https://tinyurl.com/33dm7erv");
     }
-
+    public void Add_Image(String url){
+        picasso.get()
+                .load(url).into(imageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        listImage.add(url);
+                        i = listImage.size() - 1;
+                        showImage(i);
+                        Toast.makeText(MainActivity.this
+                                , "Add Success", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public void onError(Exception e) {
+                        if(listImage.size() >0){
+                            showImage(i);
+                        }
+                        Toast.makeText(MainActivity.this
+                                , "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
     public void showImage(int position){
         picasso.get().load(listImage.get(position)).into(imageView);
     }
