f441af3ab74e188edf7ab6900e226978ae3614c2
vovietl0ng <vovielong123@gmail.com>
2022-11-19 21:14:15 +0700

    logbook1

--- app/src/main/java/com/example/logbook/MainActivity.java
+++ app/src/main/java/com/example/logbook/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.logbook;
-
 import androidx.appcompat.app.AppCompatActivity;
 import com.squareup.picasso.Picasso;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
